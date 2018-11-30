a6315c93ef187fcc17bdad04d223843d579d7da8
Brent <brentthomas.c@gmail.com>
2018-11-30 12:44:55 -0700

    omg

--- knexfile.js
+++ knexfile.js
@@ -8,13 +8,13 @@ module.exports = {
   
   // Mirror image of production => push to prod from staging
   staging: {
-    client: 'pg',
+    client: 'postgresql',
     connection: 'postgres://sonduksfvgscvq:cd550caa92873a72ad096e39272df99ff0ea59f1632229fc36f780ece6b601e1@ec2-54-235-193-0.compute-1.amazonaws.com:5432/dc4i1q8tqsens6'
 },
 
   // most stable and tested version of staging
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: 'postgres://sonduksfvgscvq:cd550caa92873a72ad096e39272df99ff0ea59f1632229fc36f780ece6b601e1@ec2-54-235-193-0.compute-1.amazonaws.com:5432/dc4i1q8tqsens6'
   } 
 
