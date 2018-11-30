2d6d91ede3446cbd7d512404e56b05fbf8564fa9
Brent <brentthomas.c@gmail.com>
2018-11-29 19:13:34 -0700

    deploying

--- knexfile.js
+++ knexfile.js
@@ -10,11 +10,12 @@ module.exports = {
   staging: {
     client: 'postgresql',
     connection: 'postgres://sonduksfvgscvq:cd550caa92873a72ad096e39272df99ff0ea59f1632229fc36f780ece6b601e1@ec2-54-235-193-0.compute-1.amazonaws.com:5432/dc4i1q8tqsens6'
-  },
+},
 
+  // most stable and tested version of staging
   production: {
     client: 'postgresql',
-    // connection: 'HEROKU'
+    connection: 'postgres://sonduksfvgscvq:cd550caa92873a72ad096e39272df99ff0ea59f1632229fc36f780ece6b601e1@ec2-54-235-193-0.compute-1.amazonaws.com:5432/dc4i1q8tqsens6'
   }
 
 };
