ebd45bc9db01e5bc54a7d45e98f9cbb422fcbdf7
Brent <brentthomas.c@gmail.com>
2018-12-28 00:27:00 -0700

    work

--- controllers/Emails/WelcomeEmail.js
+++ controllers/Emails/WelcomeEmail.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const nodeMailer = require('nodeMailer')
+const nodeMailer = require('nodemailer')
 
 router.post('/', (req, res) => {
     let formData = req.body;
