72472abc78f1247c57059ac166c3ce88b8757266
Brent <brentthomas.c@gmail.com>
2018-12-28 22:32:21 -0700

    work

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 let config = {
     secret: 'secretWord!',
     
-    SECRET_ACCESS_KEY: "Xz0xyQnSPrQ+Iprkpa9UUEkKCp9Liv5HytLNO9H6",
-    ACCESS_KEY_ID: "AKIAJBQGIS2GQ4ZHE5CA",
+    SECRET_ACCESS_KEY: "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",
+    ACCESS_KEY_ID: "AKIAJMDEFZFSGZAXPU7Q",
     BUCKET: "watchcollectionbucket",
     REGION: 'us-east-1'
 }
