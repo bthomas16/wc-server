108adc1554d2be61225c4e41617be5cfac2ec52d
Brent <brentthomas.c@gmail.com>
2019-01-02 19:35:50 -0700

    working

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -12,10 +12,10 @@ self.addEventListener('fetch', function (event) {
         caches.open('watchsoc').then(function (cache) {
           return cache.match(event.request).then(function (response) {
             return response || fetch(event.request)
-            //   .then(function (response) {
-            //     cache.put(event.request, response.clone())
-            //     return response
-            //   })
+              .then(function (response) {
+                cache.put(event.request, response.clone())
+                return response
+              })
           })
         })
       )
