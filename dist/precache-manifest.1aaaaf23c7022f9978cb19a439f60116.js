f3ada27d1de962c5518758878aae016bc33e8fde
Brent <brentthomas.c@gmail.com>
2019-01-08 13:07:02 -0700

    meta tags pls work

---
+++ dist/precache-manifest.1aaaaf23c7022f9978cb19a439f60116.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f77dd8133f335311f5a7",
+    "url": "/js/chunk-vendors.3b00a261.js"
+  },
+  {
+    "revision": "7fc964e3d4e481c62533",
+    "url": "/js/app.63a228e7.js"
+  },
+  {
+    "revision": "1aad8b60c4fa2a58da77",
+    "url": "/js/DiscoverWatches.aa371b6a.js"
+  },
+  {
+    "revision": "1661d61217f292e21a2e6eced60689a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f77dd8133f335311f5a7",
+    "url": "/css/chunk-vendors.2b24d1f6.css"
+  },
+  {
+    "revision": "7fc964e3d4e481c62533",
+    "url": "/css/app.fc2005c5.css"
+  },
+  {
+    "revision": "1aad8b60c4fa2a58da77",
+    "url": "/css/DiscoverWatches.a6bfccd0.css"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.48bc3ff9c544882c3652dce9e52d6b0c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.1aaaaf23c7022f9978cb19a439f60116.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
