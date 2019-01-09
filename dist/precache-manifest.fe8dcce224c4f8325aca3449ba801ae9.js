bb40ce22a97714d0e84a4887824afc255133373e
Brent <brentthomas.c@gmail.com>
2019-01-09 08:41:05 -0700

    ok

---
+++ dist/precache-manifest.fe8dcce224c4f8325aca3449ba801ae9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f77dd8133f335311f5a7",
+    "url": "/js/chunk-vendors.3b00a261.js"
+  },
+  {
+    "revision": "ebc266d7f091f2f775d7",
+    "url": "/js/app.ddbb5514.js"
+  },
+  {
+    "revision": "bd5695fbcb83cf22d2fd",
+    "url": "/js/DiscoverWatches.a465ba9a.js"
+  },
+  {
+    "revision": "fc978f652db2be8fa81d80fcdca30c32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f77dd8133f335311f5a7",
+    "url": "/css/chunk-vendors.2b24d1f6.css"
+  },
+  {
+    "revision": "ebc266d7f091f2f775d7",
+    "url": "/css/app.35104429.css"
+  },
+  {
+    "revision": "bd5695fbcb83cf22d2fd",
+    "url": "/css/DiscoverWatches.c9524725.css"
+  }
+];
