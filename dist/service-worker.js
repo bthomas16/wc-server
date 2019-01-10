cd3acc31629273923306df94179c84a3063395b7
Brent <brentthomas.c@gmail.com>
2019-01-09 19:54:51 -0700

    ok

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.276b4eb3d6bea08a050042feedbd0b89.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.c84fa29d949677ac6e0303b0958da89d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
