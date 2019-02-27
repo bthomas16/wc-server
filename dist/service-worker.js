10c76fe334f1f7cfde22df9702b6df1cb37a063e
Brent <brentthomas.c@gmail.com>
2019-02-27 07:55:08 -0700

    ui fix load pink bg

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.cb2fcb56274e83292cc24be32a8ace82.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.8fe534debfd829a1337e35271969836b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
