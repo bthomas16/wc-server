359bb14447f0809d352b59c91a1e817f8af1771e
Brent <brentthomas.c@gmail.com>
2019-03-19 12:43:34 -0600

    fix contact page

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.610c482c680d92dcbef8fd27cf719d87.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.4087263bb75c48f967582b25fe253eb2.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
